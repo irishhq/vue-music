2bda1e8cffb516254e1241f2eb682cae02b454d1
iris <iris_hhqian@163.com>
2019-10-23 23:41:56 +0800

    jsonp封装

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,25 @@
+import OriginJsonp from 'jsonp'
+
+export default {
+    jsonp: function (url, data, option) {
+        let url = (url.indexOf('?') < 0 ? '?' : '&') + params(data)
+        return new Promise((resolve, reject) => {
+            OriginJsonp(url, option, (err, data) => {
+                if (!err) {
+                    resolve(data)
+                } else {
+                    reject(err)
+                }
+            })
+        })
+    }
+}
+
+function params(data) {
+    let url = ''
+    for (var k in data) {
+        let value = data[k] !== 'undefined' ? data[k] : ''
+        url += `&${k}=${encodeURIComponent(value)}`/* url中含有特殊字符，将字符串作为URI组件进行编码 */
+    }
+    return url ? url.substring(1) : ''
+}
