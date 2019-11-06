3b6c125939270b266cc288cf3fed38148d67368c
iris <iris_hhqian@163.com>
2019-11-06 23:42:28 +0800

    新增推荐页面列表图片懒加载

--- src/main.js
+++ src/main.js
@@ -4,10 +4,18 @@ import App from './App'
 import router from './router'
 import 'common/stylus/index.styl'
 import fastclick from 'fastclick'
+import Lazyload from 'vue-lazyload'
 
 fastclick.attach(document.body)
 
 Vue.config.productionTip = false
+
+Vue.use(Lazyload, {
+  preLoad: 1.3, /* 预载高度比 */
+  error: require('common/image/img-down-err.png'),
+  loading: require('common/image/default.png'),
+  attempt: 1
+})
 
 /* eslint-disable no-new */
 new Vue({
