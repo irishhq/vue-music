3a6fdff25885e833c8af427f5030647fa2b89a9a
zt_huanghq <huanghuaqian@agree.com.cn>
2019-11-07 20:23:08 +0800

    新增歌手界面：数据获取&listview组件

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,9 +1,69 @@
 <template>
-  <div id="recommed">歌手页面</div>
+  <div id="singerPage" class="singer">
+    <Listview :data="singers"></Listview>
+  </div>
 </template>
 <script>
-export default {}
+import { getSingerList } from 'api/singer'
+import { ERROR_OK } from 'api/config'
+import Listview from 'base/listview/listview'
+import Singer from 'common/js/singer'
+export default {
+  data() {
+    return {
+      singers: {
+        type: Array,
+        default: () => {
+          return []
+        }
+      }
+    }
+  },
+  components: {
+    Listview
+  },
+  created() {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then((res) => {
+        if (res.code === ERROR_OK) {
+          this.singers = this.normalizeSingers(res.data.list || [])
+        }
+      })
+    },
+    normalizeSingers(list) {
+      let map = {
+        hot: {
+          title: '热门',
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < 10) {
+          map.hot.items.push(new Singer(item.Fsinger_id, item.Fsinger_name, item.Fsinger_mid))
+        }
+        let key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer(item.Fsinger_id, item.Fsinger_name, item.Fsinger_mid))
+      })
+      console.log(typeof (map), map)
+      return map
+    }
+  }
+}
 </script>
 <style scoped lang="stylus">
-
+  .singer
+    /* better-scroll父容器 */
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
