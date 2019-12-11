caf5274dbc67ed308bd0ee6b5e1978e0e405a3ce
zt_huanghq <huanghuaqian@agree.com.cn>
2019-12-11 16:36:21 +0800

    播放器数据设计

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -4,6 +4,24 @@ const mutations = {
   /* mutationstypes便于书写方便&lint检测是否正确 */
   [types.SET_SINGER](state, singer) {
     state.singer = singer
+  },
+  [types.SET_PLAYING_STATE](state, flag) {
+    state.playing = flag
+  },
+  [types.SET_FULL_SCREEN](state, flag) {
+    state.fullScreen = flag
+  },
+  [types.SET_PLAY_LIST](state, playList) {
+    state.playing = playList
+  },
+  [types.SET_SEQUENCE_LIST](state, sequenceList) {
+    state.sequenceList = sequenceList
+  },
+  [types.SET_MODE](state, mode) {
+    state.mode = mode
+  },
+  [types.SET_CURRENT_INDEX](state, currentIndex) {
+    state.currentIndex = currentIndex
   }
 }
 
