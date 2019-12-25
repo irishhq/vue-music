8be85602e811ff4b7ab4dfeee780fa047465b301
zt_huanghq <huanghuaqian@agree.com.cn>
2019-12-25 17:56:42 +0800

    新增progress-cirle

---
+++ src/base/progress-circle/progress-circle.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg :width="radius" :height="radius" viewBox="0,0,100,100">
+    <circle cx="50" cy="50" r="50" fill="transparent"></circle>
+    <circle cx="50" cy="50" r="50" fill="blue"></circle>
+  </svg>
+</template>
+<script>
+export default {
+  props: {
+    radius: {
+      type: Number,
+      default: 50
+    }
+  }
+};
+</script>
